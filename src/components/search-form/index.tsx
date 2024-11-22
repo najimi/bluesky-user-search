4f3e1f0d68b2bb73c03c4ca507226c2de731b6a4
Adrian Payne <dazulu@users.noreply.github.com>
2024-11-22 20:38:39 +0100

    feat: improve filters ux
    - animate entry/exit
    - adjust hierachy for natural tabbing
    - minor tab ux handling

--- src/components/search-form/index.tsx
+++ src/components/search-form/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import {
   Box,
   Card,
@@ -8,6 +9,7 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -15,15 +17,16 @@ import { CloseButton } from "@/components/ui/close-button";
 import { Field } from "@/components/ui/field";
 import { InputGroup } from "@/components/ui/input-group";
 import { LuFilter } from "react-icons/lu";
-import { Show } from "@chakra-ui/react";
-import { useState } from "react";
 import { useStore } from "@/store";
 
 export const SearchForm = () => {
-  const [showFilters, setShowFilters] = useState(false);
   const [formError, setFormError] = useState(false);
   const { formOptions, loading, setQuery, query, updateFormOptions } =
     useStore();
+
+  const [showFilters, setShowFilters] = useState(false);
+  const filtersButtonRef = useRef<HTMLButtonElement>(null);
+  const firstFilterElementRef = useRef<HTMLInputElement>(null);
 
   const handleOmitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormError(false);
@@ -46,14 +49,31 @@ export const SearchForm = () => {
     updateFormOptions({ omit });
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setShowFilters(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showFilters) {
+      firstFilterElementRef.current?.focus();
+    } else {
+      filtersButtonRef.current?.focus();
+    }
+  }, [showFilters]);
+
   return (
     // Extra Padding here accounting for focus rings
-    <Box paddingLeft="4px" paddingRight="4px">
+    <Box paddingLeft="4px" paddingRight="4px" position="relative">
       <Grid
-        pt="4"
+        background="white"
         gap="6px"
+        position="relative"
+        pt="4"
         templateColumns={["1fr auto", "1fr auto auto"]}
         templateRows={["auto auto", "1fr"]}
+        zIndex={3}
       >
         <InputGroup
           flex="1"
@@ -71,94 +91,124 @@ export const SearchForm = () => {
           <Input
             aria-label="Search Bluesky users"
             placeholder="Who are you looking for?"
+            size="lg"
             type="text"
             value={query}
-            size="lg"
             onInput={(e) =>
               setQuery((e.target as HTMLInputElement).value.toLocaleLowerCase())
             }
           />
         </InputGroup>
+
         <Button
-          id="filters-button"
           aria-expanded={showFilters}
           aria-label={showFilters ? "Hide filters" : "Show filters"}
+          aspectRatio="1"
+          ref={filtersButtonRef}
+          id="filters-button"
           size="lg"
           variant="outline"
-          aspectRatio="1"
           onClick={() => setShowFilters((state) => !state)}
         >
           <LuFilter size="lg" />
         </Button>
+
+        <AnimatePresence>
+          {showFilters && (
+            <motion.div
+              aria-controls="filters-button"
+              className="filters-card"
+              initial={{
+                opacity: 0,
+                y: -40,
+              }}
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+              exit={{
+                opacity: 0,
+                y: -40,
+                transition: {
+                  ease: "easeOut",
+                  duration: 0.2,
+                },
+              }}
+              onKeyDown={handleKeyDown}
+            >
+              <Card.Root mt="2" pt="0" size="sm">
+                <Card.Body>
+                  <Stack
+                    direction={["column", "row"]}
+                    gap={["4", "6"]}
+                    alignItems="stretch"
+                  >
+                    <Box>
+                      <Text fontWeight="semibold" textStyle="md">
+                        Search within
+                      </Text>
+                      <Stack direction="column" gap="1" pt="2">
+                        <Checkbox
+                          checked={formOptions.name}
+                          size="sm"
+                          ref={firstFilterElementRef}
+                          onCheckedChange={() =>
+                            updateFormOptions({ name: !formOptions.name })
+                          }
+                        >
+                          Name/Handle
+                        </Checkbox>
+                        <Checkbox
+                          checked={formOptions.description}
+                          size="sm"
+                          onCheckedChange={() =>
+                            updateFormOptions({
+                              description: !formOptions.description,
+                            })
+                          }
+                        >
+                          Bio
+                        </Checkbox>
+                      </Stack>
+                    </Box>
+                    <Flex minHeight="inherit" alignItems="stretch">
+                      <Separator orientation={["horizontal", "vertical"]} />
+                    </Flex>
+                    <Box>
+                      <Field
+                        errorText="Away with your tricks!"
+                        fontWeight="semibold"
+                        helperText={!formError && "Separate words with commas"}
+                        invalid={formError}
+                        label="Omit"
+                        textStyle="sm"
+                      >
+                        <Input
+                          id="search-input"
+                          placeholder="e.g. x.com, bot"
+                          size="xs"
+                          onChange={handleOmitChange}
+                        />
+                      </Field>
+                    </Box>
+                  </Stack>
+                </Card.Body>
+              </Card.Root>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
         <Button
-          size="lg"
-          type="submit"
-          loading={loading}
           disabled={loading || formError}
           gridColumn={["1 / -1", "auto"]}
+          loading={loading}
+          size="lg"
+          onClick={() => setShowFilters(false)}
+          type="submit"
         >
           Search
         </Button>
       </Grid>
-      <Show when={showFilters}>
-        <Card.Root aria-controls="filters-button" mt="2" pt="0" size="sm">
-          <Card.Body>
-            <Stack
-              direction={["column", "row"]}
-              gap={["4", "6"]}
-              alignItems="stretch"
-            >
-              <Box>
-                <Text fontWeight="semibold" textStyle="md">
-                  Search within
-                </Text>
-                <Stack direction="column" gap="1" pt="2">
-                  <Checkbox
-                    size="sm"
-                    checked={formOptions.name}
-                    onCheckedChange={() =>
-                      updateFormOptions({ name: !formOptions.name })
-                    }
-                  >
-                    Name/Handle
-                  </Checkbox>
-                  <Checkbox
-                    size="sm"
-                    checked={formOptions.description}
-                    onCheckedChange={() =>
-                      updateFormOptions({
-                        description: !formOptions.description,
-                      })
-                    }
-                  >
-                    Bio
-                  </Checkbox>
-                </Stack>
-              </Box>
-              <Flex minHeight="inherit" alignItems="stretch">
-                <Separator orientation={["horizontal", "vertical"]} />
-              </Flex>
-              <Box>
-                <Field
-                  invalid={formError}
-                  label="Omit"
-                  fontWeight="semibold"
-                  textStyle="sm"
-                  helperText={!formError && "Separate words with commas"}
-                  errorText="Away with your tricks!"
-                >
-                  <Input
-                    id="search-input"
-                    size="xs"
-                    placeholder="e.g. x.com, bot"
-                    onChange={handleOmitChange}
-                  />
-                </Field>
-              </Box>
-            </Stack>
-          </Card.Body>
-        </Card.Root>
-      </Show>
     </Box>
   );
 };
