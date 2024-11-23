17b7d0e9e465c62714699bc40d536c0657fb849e
Adrian Payne <dazulu@users.noreply.github.com>
2024-11-23 16:41:48 +0100

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
+
 import { App } from "./App";
 import { BlueskyLink } from "@/components/bluesky-link";
 import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { Theme } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
+
+Sentry.init({
+  dsn: "https://e85975005f8b2da1837475a4832751bd@o4508348275228672.ingest.de.sentry.io/4508348280012880",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/bluesky-user-search\.netlify\.app/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -5,5 +6,12 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
+    org: "frontendirish",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
